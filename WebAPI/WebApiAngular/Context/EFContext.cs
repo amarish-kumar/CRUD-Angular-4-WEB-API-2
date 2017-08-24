749d75cd73e0245a5f1b4b7d29ca87918c2240a5
Emanuel Meira <emanuelmeira@outlook.com>
2017-08-23 22:39:59 -0300

    Remoção de comentario em class

--- WebAPI/WebApiAngular/Context/EFContext.cs
+++ WebAPI/WebApiAngular/Context/EFContext.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
 using System.Web;
-using WebApiAngular.Models;
+using WebApiAngular.Models.Entities;
 
 namespace WebApiAngular.Context
 {
     public class EFContext : DbContext
-    {
-        //Criar um factory para contexto, assim com ta no email
-
+    {        
         public EFContext() : base("WEBAPI-ANGULAR")
         {
         }
         
         public virtual DbSet<Product> Products { get; set; }
         
-        //Factpry para contexto
+        //Factory para contexto
         public static EFContext ContextFactory()
         {            
             return new EFContext();
