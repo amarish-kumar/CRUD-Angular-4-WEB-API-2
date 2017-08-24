54f99968487aa7a1fe609ab42a954a3c806cf3ba
Emanuel Meira <emanuelmeira@outlook.com>
2017-08-23 22:42:48 -0300

    Criação da class para captura de erros do repository

---
+++ WebAPI/WebApiAngular/Models/Exceptions/RepositoryExeption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApiAngular.Models.Exceptions
+{
+    public class RepositoryException : Exception
+    {
+        public RepositoryException() { }
+        public RepositoryException(string message) : base(message) { }
+        public RepositoryException(string message, Exception inner) : base(message, inner) { }
+
+    }
+}
